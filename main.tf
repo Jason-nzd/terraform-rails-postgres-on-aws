1b36f5505efdcdaed051f765cefd5408ca43859e
Jason Burgess <jason.nzd@gmail.com>
2023-06-16 23:18:16 +1200

    cleaned up main.tf and made rds more modular

--- main.tf
+++ main.tf
@@ -1,60 +1,50 @@
-data "aws_ami" "latest-amazon-linux" {
-  most_recent = true
-  owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["al2023-ami-*"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-}
-
+# EC2 running amazon linux 2023 AMI, with shell script to install ruby on rails
 resource "aws_instance" "ec2_server" {
   ami                         = data.aws_ami.latest-amazon-linux.id
   instance_type               = var.ec2_instance_type
   key_name                    = var.ssh_key_name
   associate_public_ip_address = true
-  subnet_id                   = aws_subnet.sn-public-1.id
-  vpc_security_group_ids      = [aws_security_group.sg_web_server.id]
+  subnet_id                   = aws_subnet.public[0].id
+  vpc_security_group_ids      = [aws_security_group.web_server.id]
 
   user_data = templatefile("install_web.sh", {
     ami         = data.aws_ami.latest-amazon-linux.id
-    db_endpoint = aws_db_instance.default.endpoint
+    db_endpoint = aws_db_instance.postgres.endpoint
     db_username = var.db_username
     db_password = var.db_password
-    db_name     = var.db_name
+    db_name     = "postgres"
   })
 
   tags = {
-    Name = "Rails Server"
+    Name = "${var.project_name}-web-server"
   }
 }
 
-resource "aws_db_subnet_group" "default" {
-  name       = "main"
-  subnet_ids = [aws_subnet.sn-private-1.id, aws_subnet.sn-private-2.id]
-
-  tags = {
-    Name = "DB Subnet Group"
-  }
-}
-
-resource "aws_db_instance" "default" {
-  identifier             = "postgres-server"
-  allocated_storage      = 5
-  db_name                = var.db_name
-  engine                 = "postgres"
-  engine_version         = var.postgres_version
-  instance_class         = var.db_instance_class
+# RDS Postgres
+resource "aws_db_instance" "postgres" {
+  identifier        = "${var.project_name}-postgres"
+  allocated_storage = 5
+  db_name           = "postgres"
+  engine            = "postgres"
+  engine_version    = var.postgres_version
+  instance_class    = var.db_instance_class
+  # storage_type           = "gp3"
   db_subnet_group_name   = aws_db_subnet_group.default.name
   username               = var.db_username
   password               = var.db_password
   parameter_group_name   = "default.postgres15"
-  vpc_security_group_ids = ["${aws_security_group.sg_postgres.id}"]
+  vpc_security_group_ids = ["${aws_security_group.postgres.id}"]
   skip_final_snapshot    = true
-  publicly_accessible    = true
+  publicly_accessible    = true # Change later
+}
+
+# RDS DB subnet group
+resource "aws_db_subnet_group" "default" {
+  name       = "${var.project_name}-db-subnet-group"
+  subnet_ids = data.aws_subnets.private.ids
+
+  tags = {
+    Name = "${var.project_name}-db-subnet-group"
+  }
 }
