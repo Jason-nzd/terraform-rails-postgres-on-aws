c3399aa7940a8b1d89ff857c1d56fa039e4a8b98
Jason Burgess <jason.nzd@gmail.com>
2023-06-16 23:16:23 +1200

    moved various data to versions.tf

--- versions.tf
+++ versions.tf
@@ -4,11 +4,38 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.1.0"
+      version = "~> 5.3.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.2.1"
     }
   }
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+data "aws_ami" "latest-amazon-linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
 }
